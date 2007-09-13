a4b598db70a755664f5d8615be7d3eb574bdab52
alain.angulo <alain.angulo@cdb1b6e3-f639-0410-92fa-098859d11743>
2007-09-13 09:10:22 +0000

    Flickr stuff

---
+++ EarthNews/src/earthnews/worldwind/layers/flickr/FlickrLayer.java
@@ -0,0 +1,19 @@
+package earthnews.worldwind.layers.flickr;
+
+/**
+ * REST URL:
+ * http://api.flickr.com/services/rest/?method=flickr.test.echo&name=value
+ * 
+ * search method help: 
+ * http://www.flickr.com/services/api/flickr.photos.search.html
+ * 
+ * get image url from info:
+ * http://www.flickr.com/services/api/misc.urls.html
+ * 
+ * key: b8e57080b95dc3ff96d5956e61231961
+ * secret: 0f3ad2578b0dd264
+ */
+public class FlickrLayer
+{
+
+}
